6b60c2ea39e7b35cba66bc777941a285b0674b23
iamc0llins <tusirius4u@gmail.com>
2022-12-09 01:08:04 +0100

    Route Update

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/new', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
 
